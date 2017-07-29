7ee3abfd6972de742bffe3a2d3f8ac3d1d2fbc48
Alexander Elias <alex.steven.elias@gmail.com>
2017-07-29 09:40:27 -0700

    cahnged from vh and vw to js initail set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+var i;
 
 var path = window.location.origin + window.location.pathname;
 var menu = document.querySelector('.menu');
 
-for (var i = 0; i < menu.children.length; i++) {
+for (i = 0; i < menu.children.length; i++) {
 	if (menu.children[i].href === path) {
 		menu.children[i].classList.add('active');
 	} else {
 		menu.children[i].classList.remove('active');
 	}
+}
+
+var fills = document.querySelectorAll('.fill');
+
+for (i = 0; i < fills.length; i++) {
+	fills[i].style.minHeight = window.innerHeight + 'px';
 }
