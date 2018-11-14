b57e14bff97561e8a5c059d0bede25d51844da5d
Alexander Elias <alex.steven.elias@gmail.com>
2018-11-13 23:26:36 -0700

    wraped code in iife

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -1,27 +1,30 @@
+(function (window) { 'use strict';
 
-var path, i, l;
-var menu = document.querySelector('.menu');
-var menuIcon = document.querySelector('.menu-icon');
-var menuList = document.querySelector('.menu-list');
+	var path, i, l;
+	var menu = document.querySelector('.menu');
+	var menuIcon = document.querySelector('.menu-icon');
+	var menuList = document.querySelector('.menu-list');
 
-path = window.location.pathname.replace(/\/$/, '');
-path = path === '' ? '/' : path;
-path = window.location.origin + path;
+	path = window.location.pathname.replace(/\/$/, '');
+	path = path === '' ? '/' : path;
+	path = window.location.origin + path;
 
-if (menuIcon) {
-	for (i = 0; i < 3; i++) {
-		menuIcon.appendChild(document.createElement('div'));
+	if (menuIcon) {
+		for (i = 0; i < 3; i++) {
+			menuIcon.appendChild(document.createElement('div'));
+		}
+
+		menuIcon.addEventListener('click', function () {
+			menu.classList.toggle('active');
+		});
 	}
 
-	menuIcon.addEventListener('click', function () {
-		menu.classList.toggle('active');
-	});
-}
-
-for (i = 0, l = menuList.children.length; i < l; i++) {
-	if (menuList.children[i].href === path) {
-		menuList.children[i].classList.add('active');
-	} else {
-		menuList.children[i].classList.remove('active');
+	for (i = 0, l = menuList.children.length; i < l; i++) {
+		if (menuList.children[i].href === path) {
+			menuList.children[i].classList.add('active');
+		} else {
+			menuList.children[i].classList.remove('active');
+		}
 	}
-}
+	
+}(this));
